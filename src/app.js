3d01946e405f71ca21e016946199880cfdadcf4d
Prabir <prabirkumarpanda90@gmail.com>
2025-06-04 10:59:49 +0530

    init - database

--- src/app.js
+++ src/app.js
@@ -1,19 +1,44 @@
 const express = require('express');
-
+const connectDB = require('./config/database');
 const app = express();
+const User = require('./models/user');
 
-app.get("/",(req, res) => {
-    res.send('Welcome to the DevTinder API!');
-});
 
-app.get("/test", (req, res) => {
-    res.send('Welcome to the Test API!');
-});
 
-app.get("/help", (req, res) => {
-    res.send('Welcome to the Help API!');
-});
+app.post("/signup", async (req, res) => {
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+    const user = new User({
+        firstName : "Prabir",
+        lastName : "Panda",
+        emailId : "prabir@123",
+        password : "Razz@123"
+    })
+
+    try {
+        await user.save();
+        res.sendfile("User added to DB successfully");
+    } catch (error) {
+        console.error("error saving the user: " + error.message);   
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+connectDB()
+    .then(() => {
+        console.log('MongoDB connected...');
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+    })
+    .catch((err) => {
+        console.log("error connecting to MongoDB: ", err);
+    });
---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+
+// Connect to MongoDB
+
+const URL = "mongodb+srv://namasteDev:cchqhg1XSKqGVgpc@namastenode.ctrtrwf.mongodb.net/devTinder";
+const connectDB = async () => {
+    await mongoose.connect(URL);
+}
+
+module.exports = connectDB;
